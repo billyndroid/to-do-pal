2a01d1c4dc18218cd58016c24639782295cd0afe
billyndroid <70548591+billyndroid@users.noreply.github.com>
2025-02-27 11:56:30 +0000

    server
    server file added

---
+++ server (Backend)/routes/Task.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const Task = require('../models/Task');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+router.post('/', async (req, res) => {
+  const newTask = new Task({
+    text: req.body.text,
+  });
+  await newTask.save();
+  res.json(newTask);
+});
+
+router.delete('/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Task deleted' });
+});
+
+module.exports = router;
